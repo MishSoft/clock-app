23363b15fa957e3956bdf105f79a4d9833d04e12
MishSoft <machiavelli6927@gmail.com>
2023-12-30 21:31:44 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import Quotes from "./components/Quotes";
+
+function App() {
+  return (
+    <div style={{ backgroundImage: `url(https://img.freepik.com/free-photo/night-view-victoria-harbor-hong-kong_53876-146261.jpg)`}} className="container">
+      <Quotes/>
+    </div>
+  );
+}
+
+export default App;
